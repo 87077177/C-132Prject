27ad80edac9475790fd073b34a4482265d298d7f
mohit <75508470+Mohit-Coder99@users.noreply.github.com>
2021-07-23 13:49:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+     window.location.replace("bedroom.html");
+}
+
+function laptop(){
+     window.location.replace("laptop.html");
+}
+
+function tv(){
+     window.location.replace("tv.html");
+}
+
+function bottle(){
+     window.location.replace("bottle.html");
+}
